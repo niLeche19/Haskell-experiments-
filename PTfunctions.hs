ce600fb853cb7f477bb5aa694f7aab51afe954dc
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-01-29 02:05:42 +0000

    999thTime

--- PTfunctions.hs
+++ PTfunctions.hs
@@ -6,11 +6,12 @@ quadraticDiscriminant b a = b^2 - 4*a
 
 
 convertFtoC f = (f-32)/1.8
+
 convertCtoF c = (1.8c)+32
+
 convertTemp t s = if s == "f"
                     then (t-32) / 1.8
                     else if s == "c"
                     then 1.8*t + 32
                     else 100
                     
-a = 110
