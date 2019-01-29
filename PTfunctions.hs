ffa0eee338d9ac2f1fff3d8f411ac52d2ea7692b
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-01-29 01:54:13 +0000

    commitnumber4

--- PTfunctions.hs
+++ PTfunctions.hs
@@ -2,7 +2,15 @@ module PTfunctions where
 
 quadraticVertex a b = -b/2*a
 
-quadraticDiscriminant a b = b^2 - 4a
+quadraticDiscriminant b a = b^2 - 4*a
 
+
+convertFtoC f = (f-32)/1.8
+convertCtoF c = (1.8c)+32
+convertTemp t s = if s == "f"
+                    then (t-32) / 1.8
+                    else if s == "c"
+                    then 1.8*t + 32
+                    else 100
+                    
 a = 110
-
