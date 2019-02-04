6211979c3dcc33ac58ec1ed0357ab3a604120f7b
niLeche19 <nicholaslee@hanovernorwichschools.org>
2019-02-04 15:05:12 +0000

    commitnumberaLOT

--- PTfunctions.hs
+++ PTfunctions.hs
@@ -1,6 +1,6 @@
 module PTfunctions where
 
-quadraticVertex a b c = -b/2*a
+quadraticVertex a b c = (-b)/(2*a)
 
 quadraticDiscriminant a b c = b^2 - 4*(a*c)
 
@@ -12,11 +12,8 @@ quadraticRealSolutionQty a b c = if quadraticDiscriminant a b c == 0
 
 convertFtoC f = (f-32)/1.8
 
-convertCtoF c = (1.8c)+32
+convertCtoF c = (1.8*c)+32
 
 convertTemp t s = if s == "f"
-                    then (t-32) / 1.8
-                    else if s == "c"
-                    then 1.8*t + 32
-                    else 100
-                    
+                    then convertFtoC t
+                    else convertCtoF t
--- baby.hs
+++ baby.hs
@@ -6,3 +6,13 @@ doubleSmallNumber x = if x > 100
                         then x  
                         else x*2   
 
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]  
+
+longth xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+myNum = 2
+addTen n = n + 10
+funcPlusTwo f n = f  ( n + 2 )
+doubleIt n = n * 2
